e7d21c6c62fd305931a29acb0b7aa7f09fd31ab2
Andy Johnson <andyrjohnson82@gmail.com>
2017-11-29 14:58:08 -0800

    remove extra iteration of dom fragment in template setup by saving path during setup and walkingPath to find target during template init

--- src/index.js
+++ src/index.js
@@ -1,70 +1,88 @@
 const ELEMENT_NODE = 1;
 const TEXT_NODE = 3;
 const COMMENT_NODE = 8;
-
 const templateCache = new Map();
-
-function placeHolderComments(parent, element) {
-  if (element.nodeType === TEXT_NODE) {
-    const text = element.nodeValue;
-    const split = text.split("{{}}");
-    const end = split.length - 1;
-    const nodes = [];
-    if (split.length > 0) {
-      split.forEach((node, i) => {
-        if (node !== "") {
-          nodes.push(document.createTextNode(node));
-        }
-        if (i < end) {
-          nodes.push(document.createComment("{{}}"));
-        }
-      });
-      nodes.forEach(node => {
-        parent.insertBefore(node, element);
-      });
-      parent.removeChild(element);
-    }
-  }
-}
+const walkPath = [];
 
 function walkDOM(parent, element, fn) {
   fn(parent, element);
   if (element.childNodes.length > 0) {
     const cloneNodes = [].slice.call(element.childNodes, 0);
-    cloneNodes.forEach(child => {
+    cloneNodes.forEach((child, i) => {
+      walkPath.push(i);
       walkDOM(element, child, fn);
+      walkPath.pop();
     });
   }
 }
 
-function generateParts(exprs, parts) {
+function followPath(parent, pointer) {
+  const cPath = pointer.slice(0);
+  let element;
+  if (parent != null) {
+    while(cPath.length > 0) {
+      const raw = cPath.unshift();
+      const num = parseInt(raw);
+      if (num !== NaN) {
+        if (element == null) {
+          element = parent.childNodes[num];
+        } else {
+          element = element.childNodes[num];
+        }
+      } else {
+        element = [element, raw];
+      }
+    }
+  }
+  return element;
+}
+
+function templateSetup(parts) {
   return function(parent, element) {
-    if (exprs.length > 0) {
-      const nodeType = element.nodeType;
-      const nodeValue = element.nodeValue;
-      if (nodeType === ELEMENT_NODE) {
-        [].forEach.call(element.attributes, attr => {
-          if (attr.nodeValue === "{{}}") {
+    const nodeType = element.nodeType;
+    if (nodeType === TEXT_NODE) {
+      const text = element.nodeValue;
+      const split = text.split("{{}}");
+      const end = split.length - 1;
+      const nodes = [];
+      let cursor = 0;
+      if (split.length > 0) {
+        split.forEach((node, i) => {
+          if (node !== "") {
+            nodes.push(document.createTextNode(node));
+            cursor++;
+          }
+          if (i < end) {
+            nodes.push(document.createComment("{{}}"));
             parts.push({
               id: Symbol(),
-              target: [element, attr.nodeName],
-              expression: exprs.shift(),
+              path: walkPath.concat(cursor),
+              start: null,
               end: null,
               dispose: null
             });
+            cursor++;
           }
         });
-      } else if (nodeType === COMMENT_NODE && nodeValue === "{{}}") {
-        parts.push({
-          id: Symbol(),
-          target: element,
-          expression: exprs.shift(),
-          end: element,
-          dispose: null
+        nodes.forEach(node => {
+          parent.insertBefore(node, element);
         });
+        parent.removeChild(element);
       }
+    } else if (nodeType === ELEMENT_NODE) {
+      [].forEach.call(element.attributes, attr => {
+        if (attr.nodeValue === "{{}}") {
+          parts.push({
+            id: Symbol(),
+            path: walkPath.concat(attr.nodeName),
+            start: null,
+            end: null,
+            dispose: null
+          });
+        }
+      });
     }
-  };
+  }
 }
 
 function updateAttribute(element, name, value) {
@@ -81,39 +99,39 @@ function updateAttribute(element, name, value) {
 }
 
 function updateTextNode(part, value) {
-  const element = part.target;
+  const element = part.start;
   const parent = element.parentNode;
   if (element.nodeType === COMMENT_NODE && typeof value === "string") {
     const newNode = document.createTextNode(value);
     parent.replaceChild(newNode, element);
-    part.target = part.end = newNode;
+    part.start = part.end = newNode;
   } else if (element.nodeType === TEXT_NODE && element.nodeValue !== value) {
     element.nodeValue = value;
   }
 }
 
 function updateNode(part, value) {
-  const element = part.target;
+  const element = part.start;
   const parent = element.parentNode;
   parent.replaceChild(value, element);
-  part.target = part.end = value;
+  part.start = part.end = value;
 }
 
 function flushPart(part) {
-  if (part.target !== part.end || part.end != null) {
-    const parent = part.target.parentNode;
+  if (part.start !== part.end || part.end != null) {
+    const parent = part.start.parentNode;
     let lastNode = part.end;
     while (lastNode) {
       const nextNode = lastNode.previousSibling;
       parent.removeChild(lastNode);
-      if (nextNode !== part.target) {
+      if (nextNode !== part.start) {
         lastNode = nextNode;
       } else {
         lastNode = null;
       }
     }
   }
-  return part.target;
+  return part.start;
 }
 
 function updateArray(part, value) {
@@ -147,14 +165,14 @@ export function render(template, target = null, part = null) {
   } else if (target.nodeType === COMMENT_NODE && target === part.end) {
     template.update();
     template.fragment.content.__template = template;
-    part.target = template.fragment.content.firstChild;
+    part.start = template.fragment.content.firstChild;
     part.end = template.fragment.content.lastChild;
     parent.replaceChild(template.fragment.content, target);
   }
 }
 
 function set(part, value) {
-  const target = part.target;
+  const target = part.start;
   if (Array.isArray(target)) {
     const element = target[0];
     const name = target[1];
@@ -184,78 +202,66 @@ function isDirective(target, expression) {
   );
 }
 
-function TemplateResult(template, exprs) {
-  const parts = [];
+function TemplateResult(template, parts, exprs) {
   let disposed = false;
+  let initialized = false;
   const result = {
     template,
-    values: exprs,
+    fragment: null,
     dispose() {
       disposed = true;
       parts.forEach(part => typeof part.dispose === "function" ? part.dispose() : null);
-    }
-  };
-  let initialized = false;
-  result.update = values => {
-    if (values) {
-      result.values = values;
-    }
-    if (!initialized) {
-      result.fragment = document.importNode(template, true);
-      [].forEach.call(result.fragment.content.children, child => {
-        const cloneExprs = result.values.slice(0);
-        walkDOM(
-          result.fragment.content,
-          child,
-          generateParts(cloneExprs, parts)
-        );
-      });
-      initialized = true;
-    } else {
-      if (result.values.length === parts.length) {
-        parts.forEach((part, i) => {
-          if (
-            !isDirective(part.target, part.expression) &&
-            !part.target.__template
-          ) {
-            part.expression = result.values[i];
-          }
+    },
+    update(values) {
+      if (values == null) {
+        values = exprs;
+      } else {
+        exprs = values;
+      }
+      if (!initialized) {
+        result.fragment = document.importNode(template, true);
+        parts.forEach(part => {
+          part.start = followPath(result.fragment.content, part.path);
         });
-      } else {
-        throw new RangeError(
-          "length of values did not match template parts length"
-        );
+        initialized = true;
       }
-    }
-    parts.forEach(part => {
-      const target = part.target;
-      const expression = part.expression;
-      if (isDirective(target, expression)) {
-        expression(newValue => {
-          set(part, newValue);
-        },
-        dispose => {
-          part.dispose = dispose;
-        },
-        part.id);
-      } else {
-        set(part, expression);
-      }
-    });
+      parts.forEach((part, i) => {
+        const target = part.start;
+        const expression = values[i];
+        if (isDirective(target, expression)) {
+          expression(newValue => {
+            set(part, newValue);
+          },
+          dispose => {
+            part.dispose = dispose;
+          },
+          part.id);
+        } else {
+          set(part, expression);
+        }
+      });
+    } 
   };
+  initialized = false;
   return result;
 }
 
 export function html(strs, ...exprs) {
   const html = strs.join("{{}}");
-  let template = templateCache.get(strs);
+  let { template, parts } = templateCache.get(strs) || { template: null, parts: [] };
   if (template == null) {
     template = document.createElement("template");
     template.innerHTML = html;
-    [].forEach.call(template.content.children, child => {
-      walkDOM(template.content, child, placeHolderComments);
+    const setupFn = templateSetup(parts);
+    console.log(parts);
+    [].forEach.call(template.content.children, (child, i) => {
+      walkPath.push(i);
+      walkDOM(template.content, child, setupFn);
+      while(walkPath.length > 0) {
+        walkPath.pop();
+      }
     });
-    templateCache.set(strs, template);
+    templateCache.set(strs, { template, parts });
   }
-  return TemplateResult(template, exprs);
+  return TemplateResult(template, parts, exprs);
 }
