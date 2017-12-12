0f389fafe4b96e110ded106702c36ceb3178ae68
Andy Johnson <andyrjohnson82@gmail.com>
2017-12-12 11:35:54 -0800

    removed code comment for Java String.hashCode()

--- src/index.js
+++ src/index.js
@@ -348,21 +348,6 @@ function checkForSerialized(hash) {
   return result;
 }
 
-/*
-String.prototype.hashCode = function() {
-    var hash = 0;
-    if (this.length == 0) {
-        return hash;
-    }
-    for (var i = 0; i < this.length; i++) {
-        char = this.charCodeAt(i);
-        hash = ((hash<<5)-hash)+char;
-        hash = hash & hash; // Convert to 32bit integer
-    }
-    return hash;
-}
-*/
-
 function generateId(strs) {
   const templateStr = strs.toString();
   let hash = 0;
